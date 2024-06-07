4d94515c148ca8dfc6ea17c6ffe36a2ab1b47f1a
David Wing <wing_david@rocketmail.com>
2024-06-07 14:25:19 -0600

    Moved member context to its own card

---
+++ src/components/MemberCard/index.jsx
@@ -0,0 +1,43 @@
+import { ChoirContext } from "../../app/(dashboard)/[choirId]/ChoirContext";
+import { useState, useContext, useEffect } from "react";
+
+export default function MemberCard({ member, key }) {
+  const choir = useContext(ChoirContext);
+  return (
+    <li key={key}>
+      <button
+        type="button"
+        className="group flex w-full items-center justify-between space-x-3 rounded-full border border-gray-300 p-2 text-left shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        <span className="flex min-w-0 flex-1 items-center space-x-3">
+          <span className="block flex-shrink-0">
+            <img className="h-10 w-10 rounded-full" src={member.image} alt="" />
+          </span>
+          <span className="block min-w-0 flex-1">
+            <span className="block truncate text-sm font-medium text-gray-900">
+              {member.name}
+            </span>
+            <span className="block truncate text-sm font-medium text-gray-500">
+              {choir.admins.includes(member.userId) ? (
+                <span className="text-red-600">Admin</span>
+              ) : (
+                <span>Member</span>
+              )}
+            </span>
+          </span>
+        </span>
+        <span className="inline-flex h-10 w-10 flex-shrink-0 items-center justify-center">
+          <span
+            className={`inline-flex items-center rounded-full px-2 py-1 mr-6 text-xs font-medium ring-1 ring-inset ${
+              choir.admins.includes(member.userId)
+                ? "bg-red-50 text-red-700 ring-red-600/20"
+                : "bg-green-50 text-green-700 ring-green-600/20"
+            }`}
+          >
+            {choir.admins.includes(member.userId) ? "Admin" : "Member"}
+          </span>
+        </span>
+      </button>
+    </li>
+  );
+}
