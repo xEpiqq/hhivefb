2bd3732aafa9ac3f093206f9dc5c1ac6ff1494b8
David Wing <wing_david@rocketmail.com>
2024-06-08 20:08:59 -0600

    Messaging now lazyloads

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     return config;
   },
+  // add domain for images
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
 };
 
 export default nextConfig;
