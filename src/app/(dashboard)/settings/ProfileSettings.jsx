db079ad0fe25010532a61e6d99a4829a31abfee8
David Wing <wing_david@rocketmail.com>
2024-06-12 15:22:40 -0600

    Settings update

---
+++ src/app/(dashboard)/settings/ProfileSettings.jsx
@@ -0,0 +1,175 @@
+"use client";
+
+import { UserContext } from "@/components/UserContext";
+import { useContext, useState, useEffect } from "react";
+
+import { firebase, storage } from "@/components/Firebase";
+import {
+    getDoc,
+    updateDoc,
+    doc,
+} from "firebase/firestore";
+
+export default function ProfileSettings() {
+  const user = useContext(UserContext);
+  const [editing, setEditing] = useState(false);
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+
+  useEffect(() => {
+    setName(user.name);
+    setEmail(user.email);
+  }, [user]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setEditing(false);
+
+    updateDoc(doc(firebase, "users", user.id), {
+      name: name,
+      email: email,
+    }).then(() => {
+      alert("Profile updated");
+    });
+    
+  }
+
+  return (
+    <div className="divide-y divide-gray-200">
+      <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
+        <div>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Personal Information
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-500">
+            Use a permanent address where you can receive mail.
+          </p>
+        </div>
+
+        <form className="md:col-span-2" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
+            <div className="col-span-full flex items-center gap-x-8">
+              <img
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+                className="h-24 w-24 flex-none rounded-lg bg-gray-800 object-cover"
+              />
+              <div>
+                <button
+                  type="button"
+                  disabled={!editing}
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500  bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-0"
+                >
+                  Change avatar
+                </button>
+                <p className="mt-2 text-xs leading-5 text-gray-500">
+                  JPG, GIF or PNG. 1MB max.
+                </p>
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  type="text"
+                  name="first-name"
+                  autoComplete="given-name"
+                  disabled={!editing}
+                  className="block w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-0"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  disabled={!editing}
+                  id="email"
+                  value={email}
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-0"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md bg-gray-50 ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                    example.com/
+                  </span>
+                  <input
+                    disabled={!editing}
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6  bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-0"
+                    placeholder="janesmith"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-8 flex">
+            {!editing ? (
+              <button
+                type="button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setEditing(true);
+                }}
+                className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                Edit
+              </button>
+            ) : (
+              <>
+                <button
+                  type="submit"
+                  disabled={!editing}
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Save
+                </button>
+                <button
+                  type="button"
+                  disabled={!editing}
+                  onClick={() => setEditing(false)}
+                  className="ml-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  Cancel
+                </button>
+              </>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/app/(dashboard)/settings/page.jsx
+++ src/app/(dashboard)/settings/page.jsx
@@ -1,22 +1,29 @@
-'use client'
-import { Fragment, useState } from 'react'
-import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
+"use client";
+import { Fragment, useState, useContext } from "react";
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { UserContext } from "@/components/UserContext";
+import ProfileSettings from "./ProfileSettings";
 
 const secondaryNavigation = [
-  { name: 'Account', href: '#', current: true },
-  { name: 'Notifications', href: '#', current: false },
-  { name: 'Billing', href: '#', current: false },
-  { name: 'Teams', href: '#', current: false },
-  { name: 'Integrations', href: '#', current: false },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+  { name: "Account", href: "#", current: true },
+  { name: "Notifications", href: "#", current: false },
+  { name: "Billing", href: "#", current: false },
+  { name: "Teams", href: "#", current: false },
+  { name: "Integrations", href: "#", current: false },
+];
 
 export default function Example() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const user = useContext(UserContext);
+  const name = user.name;
+  const email = user.email;
 
   return (
     <>
@@ -53,9 +60,16 @@ export default function Example() {
                     leaveTo="opacity-0"
                   >
                     <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                      <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
+                      <button
+                        type="button"
+                        className="-m-2.5 p-2.5"
+                        onClick={() => setSidebarOpen(false)}
+                      >
                         <span className="sr-only">Close sidebar</span>
-                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                        <XMarkIcon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
                       </button>
                     </div>
                   </TransitionChild>
@@ -78,7 +92,10 @@ export default function Example() {
                 >
                   {secondaryNavigation.map((item) => (
                     <li key={item.name}>
-                      <a href={item.href} className={item.current ? 'text-indigo-600' : ''}>
+                      <a
+                        href={item.href}
+                        className={item.current ? "text-indigo-600" : ""}
+                      >
                         {item.name}
                       </a>
                     </li>
@@ -86,134 +103,16 @@ export default function Example() {
                 </ul>
               </nav>
             </header>
+            <ProfileSettings />
 
-            {/* Settings forms */}
-            <div className="divide-y divide-gray-200">
-              <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-                <div>
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-                  <p className="mt-1 text-sm leading-6 text-gray-500">
-                    Use a permanent address where you can receive mail.
-                  </p>
-                </div>
-
-                <form className="md:col-span-2">
-                  <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-                    <div className="col-span-full flex items-center gap-x-8">
-                      <img
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                        className="h-24 w-24 flex-none rounded-lg bg-gray-800 object-cover"
-                      />
-                      <div>
-                        <button
-                          type="button"
-                          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
-                        >
-                          Change avatar
-                        </button>
-                        <p className="mt-2 text-xs leading-5 text-gray-500">JPG, GIF or PNG. 1MB max.</p>
-                      </div>
-                    </div>
-
-                    <div className="sm:col-span-3">
-                      <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                        First name
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          type="text"
-                          name="first-name"
-                          id="first-name"
-                          autoComplete="given-name"
-                          className="block w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="sm:col-span-3">
-                      <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                        Last name
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          type="text"
-                          name="last-name"
-                          id="last-name"
-                          autoComplete="family-name"
-                          className="block w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="col-span-full">
-                      <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                        Email address
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          id="email"
-                          name="email"
-                          type="email"
-                          autoComplete="email"
-                          className="block w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="col-span-full">
-                      <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                        Username
-                      </label>
-                      <div className="mt-2">
-                        <div className="flex rounded-md bg-gray-50 ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-                          <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                            example.com/
-                          </span>
-                          <input
-                            type="text"
-                            name="username"
-                            id="username"
-                            autoComplete="username"
-                            className="flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
-                            placeholder="janesmith"
-                          />
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="col-span-full">
-                      <label htmlFor="timezone" className="block text-sm font-medium leading-6 text-gray-900">
-                        Timezone
-                      </label>
-                      <div className="mt-2">
-                        <select
-                          id="timezone"
-                          name="timezone"
-                          className="block w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        >
-                          <option>Pacific Standard Time</option>
-                          <option>Eastern Standard Time</option>
-                          <option>Greenwich Mean Time</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="mt-8 flex">
-                    <button
-                      type="submit"
-                      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Save
-                    </button>
-                  </div>
-                </form>
-              </div>
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              {/* Settings forms */}
 
               <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
                 <div>
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">Change password</h2>
+                  <h2 className="text-base font-semibold leading-7 text-gray-900">
+                    Change password
+                  </h2>
                   <p className="mt-1 text-sm leading-6 text-gray-500">
                     Update your password associated with your account.
                   </p>
@@ -222,7 +121,10 @@ export default function Example() {
                 <form className="md:col-span-2">
                   <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
                     <div className="col-span-full">
-                      <label htmlFor="current-password" className="block text-sm font-medium leading-6 text-gray-900">
+                      <label
+                        htmlFor="current-password"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
                         Current password
                       </label>
                       <div className="mt-2">
@@ -237,7 +139,10 @@ export default function Example() {
                     </div>
 
                     <div className="col-span-full">
-                      <label htmlFor="new-password" className="block text-sm font-medium leading-6 text-gray-900">
+                      <label
+                        htmlFor="new-password"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
                         New password
                       </label>
                       <div className="mt-2">
@@ -252,7 +157,10 @@ export default function Example() {
                     </div>
 
                     <div className="col-span-full">
-                      <label htmlFor="confirm-password" className="block text-sm font-medium leading-6 text-gray-900">
+                      <label
+                        htmlFor="confirm-password"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
                         Confirm password
                       </label>
                       <div className="mt-2">
@@ -280,48 +188,13 @@ export default function Example() {
 
               <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
                 <div>
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">Log out other sessions</h2>
+                  <h2 className="text-base font-semibold leading-7 text-gray-900">
+                    Delete account
+                  </h2>
                   <p className="mt-1 text-sm leading-6 text-gray-500">
-                    Please enter your password to confirm you would like to log out of your other sessions across all of
-                    your devices.
-                  </p>
-                </div>
-
-                <form className="md:col-span-2">
-                  <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-                    <div className="col-span-full">
-                      <label htmlFor="logout-password" className="block text-sm font-medium leading-6 text-gray-900">
-                        Your password
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          id="logout-password"
-                          name="password"
-                          type="password"
-                          autoComplete="current-password"
-                          className="block w-full rounded-md border-0 bg-gray-50 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="mt-8 flex">
-                    <button
-                      type="submit"
-                      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Log out other sessions
-                    </button>
-                  </div>
-                </form>
-              </div>
-
-              <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-                <div>
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">Delete account</h2>
-                  <p className="mt-1 text-sm leading-6 text-gray-500">
-                    No longer want to use our service? You can delete your account here. This action is not reversible.
-                    All information related to this account will be deleted permanently.
+                    No longer want to use our service? You can delete your
+                    account here. This action is not reversible. All information
+                    related to this account will be deleted permanently.
                   </p>
                 </div>
 
@@ -339,5 +212,5 @@ export default function Example() {
         </div>
       </div>
     </>
-  )
+  );
 }
