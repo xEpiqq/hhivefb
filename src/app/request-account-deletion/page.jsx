2663832bc8e0c4649f4737b0c0a6fad70e5832aa
xepiqq <jaydencrowther@gmail.com>
2024-06-25 02:50:37 -0600

    account deletion page

---
+++ src/app/request-account-deletion/page.jsx
@@ -0,0 +1,14 @@
+export default function Example() {
+  return (
+    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center">
+        <p className="text-base font-semibold leading-7 text-indigo-600">Harmony Hive</p>
+        <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-xl">Request Account Deletion</h2>
+        <h3 className="mt-2 text-xl tracking-tight text-gray-900 sm:text-xl">Email jayden@myharmonyhive.com</h3>
+           <h3>Subject: "Account Deletion Request"</h3>
+           <h3>Include your account email and password in the message</h3>
+        <p className="mt-6 text-lg leading-8 text-gray-600">We will delete your account within 24 hours.</p>
+        </div>
+    </div>
+  );
+}
