f024f475f3a418fc2ec4d0743c09227348e3c90e
xepiqq <jaydencrowther@gmail.com>
2024-07-17 01:39:49 -0600

    letseeifthisworks

--- src/app/bigboi.jsx
+++ src/app/bigboi.jsx
@@ -146,6 +146,7 @@ export default function Bigboi({ user }) {
                             </div>
                         </div>
 
+
                         <p className="mt-10 text-center text-sm text-gray-500">
                             Not a member?{' '}
                             <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
